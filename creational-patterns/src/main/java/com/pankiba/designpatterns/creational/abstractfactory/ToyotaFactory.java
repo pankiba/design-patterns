fdb5628b04d4da0c805b40d4af465715c418e3b5
pankiba <pankiba@users.noreply.github.com>
2022-11-12 20:38:52 +0530

    initial commit

---
+++ creational-patterns/src/main/java/com/pankiba/designpatterns/creational/abstractfactory/ToyotaFactory.java
@@ -0,0 +1,16 @@
+package com.pankiba.designpatterns.creational.abstractfactory;
+
+public class ToyotaFactory extends VehicleFactory {
+
+	@Override
+	public Car createCar() {
+		return new ToyotaCar();
+	}
+
+	@Override
+	public MotorCycle createMotorcycle() {
+		return new ToyotaMotorCycle();
+	}
+
+}
+ 
